a3f68f74783ba4a3b8e0a398b6fbeea4376b9444
Vitaliy-Blinov <mr.blin174@mail.ru>
2022-02-04 11:46:25 +0500

    Listing fix

--- testcmp.cpp
+++ testcmp.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include     "mycomplex.h"
+
 using namespace std;
-int main(){//тестирование класса комплексных чисел
- Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl; A=B+C;
-cout<<"A=B+C,A="<< A<<endl;
+//тестирование класса комплексных чисел
+int main()
+{
+ Complex A;
+ Complex B(-4);
+ Complex C(23.0, 45.9);
+cout << A << "," << B << "," << C << endl;
+A = B + C;
+cout << "A=B+C, A=" << A << endl;
 
-Complex M=B-C;
-cout<< "M = B - C, M = " << M << endl;cout<<"M*A="<<M*A<<endl;
-cout<<"M/4.45="<<M/4.45<<endl;
-
-    Complex D;
-  	cout<<"Enter complex number D = "; cin >>D;
- A+=C+D;
-cout << "D = " << D << "\nA = " << A << endl;return 0;}
+ Complex M;
+M = B - C;
+cout << "M=B-C, M =" << M << endl;
+cout << "M*A=" << M*A << endl;
+cout << "M/4.45=" << M/4.45 << endl;
+ Complex D;
+  	cout << "Enter complex number D = ";
+  	cin >> D;
+ A += C + D;
+cout << "D=" << D << "\nA=" << A << endl;
+return 0;
+}
